7763715b835db8b79994c3e4ea79fc39993265a4
abhudson3 <112576341+abhudson3@users.noreply.github.com>
2024-02-02 15:24:39 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace SP2024_Assignment1_ahudson
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+
+            app.Run();
+        }
+    }
+}
